f09e592027ec00b9bea4cdfc898cef1b089d2182
orun-sunny <sunny.mukut@gmail.com>
2022-07-19 18:28:21 +0600

    addedRating

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,11 +5,11 @@ import './Product.css'
 import Rating from 'react-rating';
 
 const Product = (props) => {
-    // console.log(props);
+
 
     
-    const {name,price,seller,img,stock} = props.product
-    // const element = <FontAwesomeIcon icon={faShoppingCart} />
+    const {name,price,seller,img,stock,star} = props.product
+   
     return (
         <div className='product'>
             <div>
@@ -23,9 +23,9 @@ const Product = (props) => {
             <p>price:{price} </p>
             <p><small>only :{stock} available </small> </p> 
             <Rating
-            initialRating={3}
-              emptySymbol="far fa-star"
-              fullSymbol="fas fa-star "
+            initialRating={star}
+              emptySymbol="far fa-star icon-color"
+              fullSymbol="fas fa-star icon-color "
               readonly>
 
               </Rating>
